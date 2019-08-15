1842ff8927e010783fe2dc529ec5c4718ebae741
Foogs <foogs@mail.ru>
2019-08-15 18:31:43 +0300

    1.0.0 first

---
+++ Example/Plugin.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using System.IO;
+using Sandbox;
+using VRage.Plugins;
+using VRage.Collections;
+using VRage.FileSystem;
+using VRage.Utils;
+
+namespace Example
+{
+    public class Plugin : IPlugin
+    {
+        public static Plugin PluginInstance;
+        public static MySandboxGame se_instance;
+        public void Init(object gameInstance)
+        {
+            se_instance = (MySandboxGame)gameInstance;
+            PluginInstance = this;
+            MyLog.Default.WriteLine("Example plugin loaded!");
+        }
+        public void Update()
+        {
+
+        }
+
+        public void Dispose()
+        {
+
+            MyLog.Default.WriteLine("Example plugin unloaded!");
+        }
+
+    }
+}
